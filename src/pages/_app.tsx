3743f62987edb8f7f7f0ae7a89571cec0940f4c9
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-01-22 13:43:26 -0500

    Upgrade to wagmi, viem, rainbowkit v2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,34 @@
 import '@rainbow-me/rainbowkit/styles.css'
+
+import { DefaultSeo } from 'next-seo'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { ThemeProvider } from 'styled-components'
 import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
-import { WagmiConfig } from 'wagmi'
+import { WagmiProvider } from 'wagmi'
 import type { AppProps } from 'next/app'
 
 import '@/styles/globals.css'
-import { chains, wagmiConfig } from '@/providers'
-import { DefaultSeo } from 'next-seo'
+import { wagmiConfig } from '@/providers'
 import { useIsMounted } from '@/hooks/useIsMounted'
 import SEO from '../next-seo.config'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted()
 
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <ThemeProvider theme={lightTheme}>
-        <ThorinGlobalStyles />
-        <RainbowKitProvider chains={chains}>
-          <DefaultSeo {...SEO} />
-          {isMounted && <Component {...pageProps} />}
-        </RainbowKitProvider>
-      </ThemeProvider>
-    </WagmiConfig>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={lightTheme}>
+          <ThorinGlobalStyles />
+          <RainbowKitProvider>
+            <DefaultSeo {...SEO} />
+            {isMounted && <Component {...pageProps} />}
+          </RainbowKitProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
--- src/providers.ts
+++ src/providers.ts
@@ -1,7 +1,6 @@
-import { configureChains, createConfig } from 'wagmi'
+import { createConfig, http } from 'wagmi'
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { goerli, mainnet } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { mainnet } from 'wagmi/chains'
 
 const WALLETCONNECT_ID = process.env.NEXT_PUBLIC_WALLETCONNECT_ID
 
@@ -9,21 +8,17 @@ if (!WALLETCONNECT_ID) {
   throw new Error('Missing NEXT_PUBLIC_WALLETCONNECT_ID')
 }
 
-export const chains = [mainnet, goerli]
-
-const { publicClient, webSocketPublicClient } = configureChains(chains, [
-  publicProvider(),
-])
-
 const { connectors } = getDefaultWallets({
   appName: 'ENS Frontend Template',
   projectId: WALLETCONNECT_ID,
-  chains,
 })
 
+const chains = [mainnet] as const
+
 export const wagmiConfig = createConfig({
-  autoConnect: true,
+  chains,
   connectors,
-  publicClient,
-  webSocketPublicClient,
+  transports: {
+    [mainnet.id]: http(),
+  },
 })
