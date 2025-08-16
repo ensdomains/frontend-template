65a6e2fc56e017ed44008d3090ef07bb77013324
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-15 21:39:42 -0400

    Fix tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
