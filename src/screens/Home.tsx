1263cc1d2e9f7f0da5bc99417cb930abcd83c28d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-15 23:27:17 -0400

    Add wagmi, shadcn

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,17 @@
 export function Home() {
-  return <h1 className="">Hello World</h1>
+  return (
+    <div className="container mx-auto py-10 text-center">
+      <header className="flex flex-col gap-2">
+        <h1 className="text-4xl font-bold">ENS Frontend Examples</h1>
+        <p className="text-lg">
+          This is a collection of examples of how to use ENS in frontend
+          applications.
+        </p>
+      </header>
+
+      <main>
+        <h2>Examples</h2>
+      </main>
+    </div>
+  )
 }
