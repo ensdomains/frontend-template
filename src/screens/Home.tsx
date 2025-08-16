1aa2ee7f42896f25ef646913c876095aef44aade
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-16 00:20:58 -0400

    Setup home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export function Home() {
   return (
     <div className="container mx-auto py-10 text-center">
@@ -9,9 +20,43 @@ export function Home() {
         </p>
       </header>
 
-      <main>
-        <h2>Examples</h2>
+      <main className="mt-10">
+        <div className="grid grid-cols-2 gap-4">
+          <ExampleCard
+            href="/"
+            title="Name/Address Input"
+            description="Every address input should also accept ENS names."
+          />
+
+          <ExampleCard
+            href="/"
+            title="ENS Profile"
+            description="Show the primary and avatar for an ENS name."
+          />
+        </div>
       </main>
     </div>
+  )
+}
+
+type Props = {
+  title: string
+  description: string
+  href: string
+}
+
+function ExampleCard({ title, description, href }: Props) {
+  return (
+    <Card className="gap-3">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button asChild>
+          <Link to={href}>View Example</Link>
+        </Button>
+      </CardContent>
+    </Card>
   )
 }
