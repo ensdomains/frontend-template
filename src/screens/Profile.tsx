23e213b28a833bde5435bf3361a03cba46e6fbd4
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-11-21 16:01:06 -0300

    Improve optimistic L2 primary name page

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
-import { type Address, zeroAddress } from 'viem'
-import { useAccount, useEnsAvatar } from 'wagmi'
-import { arbitrum, base, linea, mainnet, optimism } from 'wagmi/chains'
+import {
+  type Address,
+  type Chain,
+  type Hex,
+  isAddress,
+  zeroAddress,
+} from 'viem'
+import { useEnsAvatar } from 'wagmi'
+import {
+  arbitrum,
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+  mainnet,
+  optimism,
+  optimismSepolia,
+  sepolia,
+} from 'wagmi/chains'
 
-import { ConnectButton } from '@/components/ConnectButton'
 import {
   Card,
   CardContent,
@@ -11,54 +25,19 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 import { Layout } from '@/components/ui/layout'
 import { GitHubFooter } from '@/components/ui/link'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { useEnsNameOptimistic } from '@/hooks/useEnsNameOptimistic'
 import { cn, truncateAddress } from '@/lib/utils'
 
+const l1Chains = [mainnet, sepolia]
+const mainnetChains = [mainnet, base, arbitrum, optimism]
+const testnetChains = [sepolia, baseSepolia, arbitrumSepolia, optimismSepolia]
+
 export function ProfileScreen() {
-  const { address, isConnected } = useAccount()
-  const [l1ChainId] = useState(mainnet.id)
-
-  const { data: name } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: l1ChainId,
-  })
-  const { data: defaultName } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: 0,
-  })
-  const { data: baseName } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: base.id,
-  })
-  const { data: arbName } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: arbitrum.id,
-  })
-  const { data: opName } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: optimism.id,
-  })
-  const { data: lineaName } = useEnsNameOptimistic({
-    address,
-    l1ChainId,
-    l2ChainId: linea.id,
-  })
-
-  const names = new Map([
-    ['Default', defaultName],
-    ['Ethereum', name],
-    ['Base', baseName],
-    ['Arbitrum', arbName],
-    ['Optimism', opName],
-    ['Linea', lineaName],
-  ])
+  const [address, setAddress] = useState<string | undefined>()
 
   return (
     <Layout className="max-w-xl">
@@ -66,30 +45,92 @@ export function ProfileScreen() {
         <CardHeader>
           <CardTitle>ENS Profile</CardTitle>
           <CardDescription>
-            Connect your wallet to see your ENS profile on different chains.
+            Enter an address to see the primary name on different chains. This
+            is an optimistic version of{' '}
+            <a
+              target="_blank"
+              href="https://primary.ens.domains/"
+              className="hover:text-foreground/80 underline"
+            >
+              primary.ens.domains
+            </a>
+            .
           </CardDescription>
         </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          {isConnected ? (
-            <div className="flex w-full flex-col gap-3">
-              {Array.from(names.entries()).map(([chain, name]) => (
-                <div
-                  key={chain}
-                  className="flex items-center justify-between gap-4"
-                >
-                  <span>{chain}</span>
-                  <Profile name={name} address={address} />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <ConnectButton />
+        <CardContent className="flex flex-col gap-4">
+          <Input
+            placeholder="0x1234..."
+            name="address"
+            onChange={(e) => setAddress(e.target.value.trim())}
+          />
+
+          {isAddress(address as Hex) && (
+            <Tabs defaultValue={l1Chains[0].name}>
+              <TabsList className="w-full">
+                {l1Chains.map((chain) => (
+                  <TabsTrigger key={chain.id} value={chain.name}>
+                    {chain.name}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+
+              <TabsContent
+                value="Ethereum"
+                className="flex w-full flex-col gap-3"
+              >
+                {mainnetChains.map((chain) => (
+                  <Row
+                    key={chain.id}
+                    l1Chain={mainnet}
+                    l2Chain={chain}
+                    address={address as Address}
+                  />
+                ))}
+              </TabsContent>
+
+              <TabsContent
+                value="Sepolia"
+                className="flex w-full flex-col gap-3"
+              >
+                {testnetChains.map((chain) => (
+                  <Row
+                    key={chain.id}
+                    l1Chain={sepolia}
+                    l2Chain={chain}
+                    address={address as Address}
+                  />
+                ))}
+              </TabsContent>
+            </Tabs>
           )}
         </CardContent>
       </Card>
 
       <GitHubFooter href="https://github.com/ensdomains/frontend-template/blob/main/src/screens/Input.tsx" />
     </Layout>
+  )
+}
+
+function Row({
+  l1Chain,
+  l2Chain,
+  address,
+}: {
+  l1Chain: Chain
+  l2Chain: Chain
+  address: Address
+}) {
+  const { data: name } = useEnsNameOptimistic({
+    address,
+    l1ChainId: l1Chain.id as (typeof l1Chains)[number]['id'],
+    l2ChainId: l2Chain.id,
+  })
+
+  return (
+    <div className="flex items-center justify-between gap-4">
+      <span>{l2Chain.name}</span>
+      <Profile name={name} address={address} />
+    </div>
   )
 }
 
